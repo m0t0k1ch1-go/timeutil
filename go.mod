1b2365c5fdf799252baf1ac2ec91a7ff88b06285
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-10-06 19:30:36 +0900

    feat: implement timestamp

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/m0t0k1ch1-go/timeutil
 
 go 1.21
+
+require github.com/pkg/errors v0.9.1
--- timestamp.go
+++ timestamp.go
@@ -1,1 +1,66 @@
 package timeutil
+
+import (
+	"database/sql/driver"
+	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type Timestamp struct {
+	t time.Time
+}
+
+func Now() Timestamp {
+	return Timestamp{
+		t: time.Now(),
+	}
+}
+
+func (t Timestamp) Time() time.Time {
+	return t.t
+}
+
+func (t Timestamp) String() string {
+	return strconv.FormatInt(t.t.Unix(), 10)
+}
+
+func (t Timestamp) Value() (driver.Value, error) {
+	return t.t.Unix(), nil
+}
+
+func (t *Timestamp) Scan(src any) error {
+	switch v := src.(type) {
+	case int64:
+		t.t = time.Unix(v, 0).In(time.UTC)
+
+	case []byte:
+		i, err := strconv.ParseInt(string(v), 10, 64)
+		if err != nil {
+			return errors.Wrapf(err, "failed to convert %s into type int64", string(v))
+		}
+
+		t.t = time.Unix(i, 0).In(time.UTC)
+
+	default:
+		return errors.Errorf("unexpected src type: %T", src)
+	}
+
+	return nil
+}
+
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.FormatInt(t.t.Unix(), 10)), nil
+}
+
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+	i, err := strconv.ParseInt(string(b), 10, 64)
+	if err != nil {
+		return errors.Wrapf(err, "failed to convert %s into type int64", string(b))
+	}
+
+	t.t = time.Unix(i, 0).In(time.UTC)
+
+	return nil
+}
