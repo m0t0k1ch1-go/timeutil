f532cfc4460a33754afa355e0b75f39ec35ec5ff
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-10-11 23:42:21 +0900

    test: add test for clock

---
+++ clock_test.go
@@ -0,0 +1,37 @@
+package timeutil_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/m0t0k1ch1-go/timeutil"
+	"github.com/m0t0k1ch1-go/timeutil/internal/testutil"
+)
+
+func TestClock(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		clk := timeutil.NewClock()
+
+		ts := clk.Now()
+
+		time.Sleep(1 * time.Second)
+
+		testutil.Equal(t, true, clk.Now().Time().After(ts.Time()))
+	})
+}
+
+func TestMockClock(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		ts := timeutil.Now()
+
+		clk := timeutil.NewMockClock(ts)
+
+		testutil.Equal(t, ts, clk.Now(), cmp.AllowUnexported(timeutil.Timestamp{}))
+
+		time.Sleep(1 * time.Second)
+
+		testutil.Equal(t, ts, clk.Now(), cmp.AllowUnexported(timeutil.Timestamp{}))
+	})
+}
